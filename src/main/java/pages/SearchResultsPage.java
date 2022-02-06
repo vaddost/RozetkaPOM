0de70ae95a7d7d47f83fa90002ca70de975f3477
Vadym Ostapchuk <ostvadym@gmail.com>
2022-02-06 18:44:50 +0200

    refactoring

--- src/main/java/pages/SearchResultsPage.java
+++ src/main/java/pages/SearchResultsPage.java
@@ -1,28 +1,28 @@
 package pages;
 
-import elements.Checkbox;
-import elements.TextInput;
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import elements.Element;
+import elements.TextInput;
 
-public class SearchResultsPage extends BasePage{
+import java.util.List;
+
+public class SearchResultsPage extends BasePage {
 
     @FindBy(xpath = "//div[@data-filter-name='producer']//input[@name='searchline']")
     private TextInput brandFilterSearchInput;
 
-    @FindBy(xpath = "//div[@data-filter-name='producer']//ul//a[@class='checkbox-filter__link']//input")
-    private List<Checkbox> brandFilterCheckboxesList;
+    @FindBy(xpath = "//div[@data-filter-name='producer']//a[@class='checkbox-filter__link']")
+    private List<Element> brandFilterCheckboxesList;
 
     @FindBy(css = ".catalog-settings__sorting > select")
     private WebElement sortDropdown;
 
-    @FindBy(xpath = "//div[@data-filter-name='sell_status']//input")
-    private List<Checkbox> sellStatusFilterCheckboxesList;
+    @FindBy(xpath = "//div[@data-filter-name='sell_status']//a[@class='checkbox-filter__link']")
+    private List<Element> sellStatusFilterCheckboxesList;
 
     @FindBy(css = "ul.catalog-grid > li:first-child a.goods-tile__heading")
     private WebElement firstProductTileHeadingLink;
@@ -39,10 +39,9 @@ public class SearchResultsPage extends BasePage{
         int iteration = 1;
         while (true) {
             try {
-                System.out.println(brandFilterCheckboxesList.get(0).getLabelValue());
                 brandFilterCheckboxesList.stream()
-                        .filter(x -> x.getAttribute("id").equals(brandName))
-                        .findFirst().orElseThrow().check();
+                        .filter(x -> x.getAttribute("data-id").equals(brandName))
+                        .findFirst().orElseThrow().click();
                 break;
             } catch (Exception e) {
                 if (iteration > 2){
@@ -65,9 +64,9 @@ public class SearchResultsPage extends BasePage{
         while (true){
             try{
                 sellStatusFilterCheckboxesList.stream()
-                        .filter(x -> x.findElement(By.xpath("./parent::a")).getAttribute("href")
+                        .filter(x -> x.getAttribute("href")
                                 .contains("sell_status=available"))
-                        .findFirst().orElseThrow().check();
+                        .findFirst().orElseThrow().click();
                 break;
             } catch (Exception e){
                 if (iteration > 2){
@@ -95,12 +94,5 @@ public class SearchResultsPage extends BasePage{
                 iteration++;
             }
         }
-    }
-
-    public List<String> getSellStatusCheckboxValues(){
-        waitUntilVisibilityOfElement(firstProductTileHeadingLink);
-        return sellStatusFilterCheckboxesList.stream()
-                .map(Checkbox::getLabelValue)
-                .collect(Collectors.toList());
     }
 }
